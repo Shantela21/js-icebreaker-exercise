c05952997562591afe1ba6f431c1b119533efe32
Melokuhle <melokuhlemaphisa99@gmail.com>
2025-08-08 12:15:37 +0200

    Done with number 1-4

---
+++ icebreaker.js
@@ -0,0 +1,18 @@
+let movie = "Straw";
+console.log("My favorite movie is " +  movie + ".");
+
+let siblings = 10;
+console.log("I have " + siblings + " siblings");
+
+let hasSeenMovie = false;
+console.log("Have i seen a movie? " + hasSeenMovie);
+
+let dreamVacSpot = {
+    country: "England",
+    city : "liverpool",
+    activity : "Watching football",
+}
+
+console.log("My dream vacation is to visit " + dreamVacSpot.country + ","
+   + dreamVacSpot.city + " for " + dreamVacSpot.activity + "."
+);
