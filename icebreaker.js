c75a3da514b0fa4ce36945d8b7b71ceb1bc21bcf
Shantela21 <shantelaslie@gmail.com>
2025-08-08 12:38:46 +0200

    Add Partner 2 variables

--- icebreaker.js
+++ icebreaker.js
@@ -16,3 +16,15 @@ let dreamVacSpot = {
 console.log("My dream vacation is to visit " + dreamVacSpot.country + ","
    + dreamVacSpot.city + " for " + dreamVacSpot.activity + "."
 );
+
+let favoriteFood = ["pizza", "samp", "chocolate"];
+console.log("My top 3 favorite food are: " + favoriteFood.join(",") +"." );
+
+let discontCode = "";
+console.log("User's discount code: " + (discontCode || "none"));
+
+let profilePictureUrl = null;
+console.log("User's profile URL is: " + profilePictureUrl);
+
+let galacticStarID = 12345678901234566791324567890n;
+console.log("Galactic star ID is: " + galacticStarID);
